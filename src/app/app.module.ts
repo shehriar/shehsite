57e30bff860550a5994de991b73054e48fa1f3f9
shehriar <shehriar1burney@gmail.com>
2024-02-13 05:38:40 +0500

    Removed routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { EducationComponent } from './education/education/education.component';
 import { ProjectsComponent } from './projects/projects/projects.component';
-
-const routes: Routes = [
-  { path: '', component: AppComponent },
-];
 
 @NgModule({
   declarations: [
@@ -23,9 +17,8 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
   ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
